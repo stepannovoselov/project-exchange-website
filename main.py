829097466b84e97420cdf4efc8596417b46d3b19
saatbeginbekov <saatbeginbekov@gmail.com>
2024-01-20 01:17:33 +0300

    ошибка при регистрации

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 connection = sqlite3.connect('itsallgoodman.db')
 cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS users
-             (id INTEGER PRIMARY KEY, mail TEXT)''')
+             (id INTEGER PRIMARY KEY, mail TEXT, username TEXT, password TEXT, stasus TEXT, loged INTENGER)''')
 
 
 @app.route('/')
@@ -40,11 +40,29 @@ def log():
 
     # status и mail - это аргументы, которые мы получаем из http
     # В них содержится инфомарция о статусе авторизации и о почте пользователя
-    # cursor.execute('INSERT INTO users (mail) VALUE(?)', (mail))
+    cursor.execute('INSERT INTO users (mail) VALUE(?)', (mail))
 
     return ' '.join([mail, result])
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-app.run(port=port, debug=debug)
+@app.route('/register')
+def reg():
+    return render_template('register.html')
+
+@app.route('/register', methods = ['post'])
+def register():
+    session['username'] = request.form.get('username')
+    session['password'] = request.form.get('password')
+    cursor.execute("INSERT INTO users VALUES (0, 'loh@gmail.com', session['username'], session['password'], 'loh', 1")
+    if session['login'] and session['password']:
+        session['loged1'] = 1
+        return '<html><body>Boo!</body></html>'
+
+
+
+app.run(port=1565, debug=True)
 connection.commit()
 connection.close()
