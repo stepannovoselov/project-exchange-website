4948b3e56cc2bdb9ad198565d1e8eee8520f5848
step <a>
2024-01-19 18:03:29 +0300

    Revert "добавил базы данных и чуть-чуть в питоне в-2"
    This reverts commit f0b4c7cc

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import *
 from config import *
 import uuid
+import sqlite3
 
 # Импортируем Flask и библиотеку для генерации UUID
 
 app = Flask(__name__)
 
-
+connection = sqlite3.connect('itsallgoodman.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE users
+             (id INTEGER PRIMARY KEY, mail TEXT)''')
 @app.route('/')
 def index():
     token = str(uuid.uuid4())
@@ -15,7 +19,7 @@ def index():
     # Затем мы показываем пользователю HTML-файл, передавая токен в Jinjia
 
     # TODO: На релизе убрать порт и заменить url=url, где url - ссылка на нужную страницу
-    return render_template('account.html', token=token, port=port)
+    return render_template('index.html', token=token, port=port)
 
 
 # Страница /log вызывается тогда, когда пользователь завершил авторизацию
@@ -27,8 +31,10 @@ def log():
 
     # status и mail - это аргументы, которые мы получаем из http
     # В них содержится инфомарция о статусе авторизации и о почте пользователя
+    cursor.execute('INSERT INTO users (mail) VALUE(?)', (mail))
 
     return ''.join([mail, result])
 
-
-app.run(port=port, debug=debug)
+app.run(port=port)
+connection.commit()
+connection.close()
