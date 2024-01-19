f692c13db64c48c46aa0e2cf4647aa90337af1de
step <a>
2024-01-19 18:53:16 +0300

    Улучшения в коде

--- main.py
+++ main.py
@@ -9,23 +9,20 @@ app = Flask(__name__)
 
 connection = sqlite3.connect('itsallgoodman.db')
 cursor = connection.cursor()
-cursor.execute('''CREATE TABLE users
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
              (id INTEGER PRIMARY KEY, mail TEXT)''')
 
 
 @app.route('/')
 def index():
-    if session == False:  # TODO: Если сессия отсутствует, то переправлять пользователя на страницу регистрации
-        token = str(uuid.uuid4())
+    token = str(uuid.uuid4())
 
-        # В главной функции мы каждый раз генерируем UUID в формате строки
-        # Затем мы показываем пользователю HTML-файл, передавая токен в Jinjia
+    # В главной функции мы каждый раз генерируем UUID в формате строки
+    # Затем мы показываем пользователю HTML-файл, передавая токен в Jinjia
 
-        # TODO: На релизе убрать порт и заменить url=url, где url - ссылка на нужную страницу
-        link = 'http://127.0.0.1:' + port + '/'
-        return render_template('login.html', token=token, link=link)
-    else:  # TODO: Если сессия существует, отправлять пользователю основную страницу
-        return 'else page'
+    # TODO: На релизе убрать порт и заменить url=url, где url - ссылка на нужную страницу
+    link = 'http://127.0.0.1:' + str(port) + '/page'
+    return render_template('login.html', token=token, link=link)
 
 
 # Страница /log вызывается тогда, когда пользователь завершил авторизацию
@@ -37,11 +34,11 @@ def log():
 
     # status и mail - это аргументы, которые мы получаем из http
     # В них содержится инфомарция о статусе авторизации и о почте пользователя
-    cursor.execute('INSERT INTO users (mail) VALUE(?)', (mail))
+    # cursor.execute('INSERT INTO users (mail) VALUE(?)', (mail))
 
     return ''.join([mail, result])
 
 
-app.run(port=port)
+app.run(port=port, debug=debug)
 connection.commit()
 connection.close()
